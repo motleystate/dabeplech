8aa8a5e446887a91ada4c6cc63ea06ef2e52123d
Kenzo-Hugo Hillion <hillion.kenzo@gmail.com>
2019-12-06 11:24:20 +0100

    Allow JWT auth for metagenedb

--- bioapi/metagenedb.py
+++ bioapi/metagenedb.py
@@ -4,9 +4,13 @@ from .base import BaseAPI
 class MetageneDBAPI(BaseAPI):
     BASE_URL = 'http://localhost/'
 
-    def __init__(self, base_url=BASE_URL):
+    def __init__(self, base_url=BASE_URL, jwt_token=None):
         self.base_url = base_url
         super().__init__()
+        if jwt_token is not None:
+            self.session.headers.update({
+                'Authorization': f"JWT {jwt_token}"
+            })
 
 
 class MetageneDBCatalogGeneAPI(MetageneDBAPI):
